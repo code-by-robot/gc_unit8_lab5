f99cc53a316b20cc55b9c0507e8f83b23c7bdeec
robot <rahaupt0@gmail.com>
2022-08-04 16:28:24 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToDo } from './to-do';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  title = 'To-Do List';
+
+  sampleItems: ToDo[] = [
+    {
+      task:"Take out trash",
+      completed: false
+    },
+    {
+      task:"Sweep kitchen",
+      completed: true
+    }
+  ];
+
+  CompleteTask(ToDo:ToDo){
+    ToDo.completed = (ToDo.completed == true) ? false: true;
+  }
+
+  addItem(form:NgForm):void{
+    let newItem:ToDo={
+      task: form.form.value.task,
+      completed: false
+    }
+    this.sampleItems.push(newItem);
+  }
 }
